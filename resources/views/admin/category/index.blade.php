12ebfe7efeaf74aebd94b00e13e5fcc664d07004
MiftahFA <miftahfariedh17131@gmail.com>
2023-11-16 11:29:24 +0700

    Bag 7: 76

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,153 @@
+@extends('admin.layouts.master')
+<link rel="stylesheet" href="{{ asset('admin/assets/modules/datatables/datatables.min.css') }}">
+<link rel="stylesheet"
+    href="{{ asset('admin/assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css') }}">
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>{{ __('Categories') }}</h1>
+        </div>
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4>{{ __('All Categories') }}</h4>
+                <div class="card-header-action">
+                    <a href="{{ route('admin.category.create') }}" class="btn btn-primary">
+                        <i class="fas fa-plus"></i> {{ __('Create new') }}
+                    </a>
+                </div>
+            </div>
+            <div class="card-body">
+                <ul class="nav nav-tabs" id="myTab2" role="tablist">
+                    @foreach ($languages as $language)
+                        <li class="nav-item">
+                            <a class="nav-link {{ $loop->index === 0 ? 'active' : '' }}" id="home-tab2" data-toggle="tab"
+                                href="#home-{{ $language->lang }}" role="tab" aria-controls="home"
+                                aria-selected="true">{{ $language->name }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="tab-content tab-bordered" id="myTab3Content">
+                    @foreach ($languages as $language)
+                        @php
+                            $categories = \App\Models\Category::where('language', $language->lang)
+                                ->orderByDesc('id')
+                                ->get();
+                        @endphp
+                        <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}"
+                            id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped" id="table-{{ $language->lang }}">
+                                        <thead>
+                                            <tr>
+                                                <th class="text-center">
+                                                    #
+                                                </th>
+                                                <th>{{ __('Name') }}</th>
+                                                <th>{{ __('Language Code') }}</th>
+                                                <th>{{ __('In Nav') }}</th>
+                                                <th>{{ __('Status') }}</th>
+                                                <th>{{ __('Action') }}</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @php
+                                                $count = 1;
+                                            @endphp
+                                            @foreach ($categories as $category)
+                                                <tr>
+                                                    <td class="text-center">{{ $count++ }}</td>
+                                                    <td>{{ $category->name }}</td>
+                                                    <td>{{ $category->language }}</td>
+                                                    <td>
+                                                        @if ($category->show_at_nav == 1)
+                                                            <span class="badge badge-primary">{{ __('Yes') }}</span>
+                                                        @else
+                                                            <span class="badge badge-danger">{{ __('No') }}</span>
+                                                        @endif
+
+                                                    </td>
+                                                    <td>
+                                                        @if ($category->status == 1)
+                                                            <span class="badge badge-success">{{ __('Yes') }}</span>
+                                                        @else
+                                                            <span class="badge badge-danger">{{ __('No') }}</span>
+                                                        @endif
+
+                                                    </td>
+                                                    <td>
+                                                        <a href="{{ route('admin.category.edit', $category->id) }}"
+                                                            class="btn btn-primary"><i class="fas fa-edit"></i></a>
+                                                        <a href="{{ route('admin.category.destroy', $category->id) }}"
+                                                            class="btn btn-danger delete-item"><i
+                                                                class="fas fa-trash-alt"></i></a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@push('scripts')
+    <script src="{{ asset('admin/assets/modules/datatables/datatables.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js') }}">
+    </script>
+    <script src="{{ asset('admin/assets/modules/sweetalert/sweetalert.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            @foreach ($languages as $language)
+                $("#table-{{ $language->lang }}").DataTable({
+                    "columnDefs": [{
+                        "sortable": false,
+                        "targets": [2, 3]
+                    }]
+                });
+            @endforeach
+
+            $('.delete-item').on('click', function(e) {
+                e.preventDefault();
+                swal({
+                        title: 'Are you sure?',
+                        text: "You won't be able to revert this!",
+                        icon: 'warning',
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            let url = $(this).attr('href');
+                            $.ajax({
+                                method: 'DELETE',
+                                url: url,
+                                success: function(data) {
+                                    if (data.status === 'success') {
+                                        swal({
+                                            title: data.message,
+                                            icon: 'success',
+                                        }).then(() => {
+                                            location.reload();
+                                        });
+                                    } else if (data.status === 'error') {
+                                        swal(data.message, {
+                                            icon: 'error',
+                                        });
+                                    }
+                                },
+                                error: function(xhr, status, error) {
+                                    console.error(error);
+                                }
+                            });
+                        }
+                    });
+            });
+        });
+    </script>
+@endpush

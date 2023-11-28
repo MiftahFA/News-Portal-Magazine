47241685b72709037341866a915c8b888f5f6765
= <=>
2023-11-28 16:55:07 +0700

    Bag 12: 116

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -16,7 +16,80 @@ class HomeController extends Controller
     {
         $breakingNews = News::where(['is_breaking_news' => 1,])
             ->activeEntries()->withLocalize()->orderBy('id', 'DESC')->take(10)->get();
-        return view('frontend.home', compact('breakingNews'));
+
+        $heroSlider = News::with(['category', 'auther'])
+            ->where('show_at_slider', 1)
+            ->activeEntries()
+            ->withLocalize()
+            ->orderBy('id', 'DESC')->take(7)
+            ->get();
+
+        $recentNews = News::with(['category', 'auther'])->activeEntries()->withLocalize()
+            ->orderBy('id', 'DESC')->take(6)->get();
+
+        $popularNews = News::with(['category'])->where('show_at_popular', 1)
+            ->activeEntries()->withLocalize()
+            ->orderBy('updated_at', 'DESC')->take(4)->get();
+
+        // $HomeSectionSetting = HomeSectionSetting::where('language', getLangauge())->first();
+
+        // if ($HomeSectionSetting) {
+        //     $categorySectionOne = News::where('category_id', $HomeSectionSetting->category_section_one)
+        //         ->activeEntries()->withLocalize()
+        //         ->orderBy('id', 'DESC')
+        //         ->take(8)
+        //         ->get();
+
+        //     $categorySectionTwo = News::where('category_id', $HomeSectionSetting->category_section_two)
+        //         ->activeEntries()->withLocalize()
+        //         ->orderBy('id', 'DESC')
+        //         ->take(8)
+        //         ->get();
+
+        //     $categorySectionThree = News::where('category_id', $HomeSectionSetting->category_section_three)
+        //         ->activeEntries()->withLocalize()
+        //         ->orderBy('id', 'DESC')
+        //         ->take(6)
+        //         ->get();
+
+        //     $categorySectionFour = News::where('category_id', $HomeSectionSetting->category_section_four)
+        //         ->activeEntries()->withLocalize()
+        //         ->orderBy('id', 'DESC')
+        //         ->take(4)
+        //         ->get();
+        // } else {
+        //     $categorySectionOne = collect();
+        //     $categorySectionTwo = collect();
+        //     $categorySectionThree = collect();
+        //     $categorySectionFour = collect();
+        // }
+
+
+        $mostViewedPosts = News::activeEntries()->withLocalize()
+            ->orderBy('views', 'DESC')
+            ->take(3)
+            ->get();
+
+        // $socialCounts = SocialCount::where(['status' => 1, 'language' => getLangauge()])->get();
+
+        $mostCommonTags = $this->mostCommonTags();
+
+        // $ad = Ad::first();
+
+        return view('frontend.home', compact(
+            'breakingNews',
+            'heroSlider',
+            'recentNews',
+            'popularNews',
+            // 'categorySectionOne',
+            // 'categorySectionTwo',
+            // 'categorySectionThree',
+            // 'categorySectionFour',
+            'mostViewedPosts',
+            // 'socialCounts',
+            'mostCommonTags',
+            // 'ad'
+        ));
     }
 
     public function ShowNews(string $slug)
